ee5dc5d93d42c0cce0dde52f1c77d7a8263dd2ad
jstm17 <62430328+jstm17@users.noreply.github.com>
2023-06-21 16:51:50 +0200

    fix menu + design

--- src/script.js
+++ src/script.js
@@ -3,20 +3,6 @@ import {
     OrbitControls
 } from "three/examples/jsm/controls/OrbitControls.js";
 
-// Time / Pause
-
-const pauseBtn = document.querySelector('.pause-btn');
-
-pauseBtn.addEventListener('click', function() {
-    pauseBtn.classList.toggle('stop');
-
-    if (pauseBtn.classList.contains('stop')) {
-        pauseBtn.innerHTML = "Resume";
-      } else {
-        pauseBtn.innerHTML = "Stop";
-      }
-})
-
 // Scene 
 const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
@@ -288,10 +274,23 @@ function onClick(event) {
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+
+// Time / Pause
+
+const pauseBtn = document.querySelector('.pause-btn');
+
+pauseBtn.addEventListener('click', function() {
+    pauseBtn.classList.toggle('stop');
+
+    if (pauseBtn.classList.contains('stop')) {
+        pauseBtn.innerHTML = '<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />';
+      } else {
+        pauseBtn.innerHTML = '<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />';
+      }
+})
+
 let seconde = 0;
-
 let years = 0;
-
 
 
 // Animate
@@ -317,7 +316,7 @@ const animate = () => {
     }
 
     // Affichage du nombre d'années
-    document.querySelector('.year').innerHTML = Math.round(years) + " years passed";
+    document.querySelector('.year').innerHTML = Math.round(years) + " années passées";
 
     // Rotation planète + anneaux autour du soleil  
     planetsMesh["mercury"].position.x = Math.cos( seconde * 25 ) * planets[1].posX;
