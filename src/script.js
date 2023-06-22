a7ff12463487d6bae53e236ce9df76345d8b691b
Wintxer <47366828+Wintxer@users.noreply.github.com>
2023-06-22 12:45:00 +0200

    zoom

--- src/script.js
+++ src/script.js
@@ -334,11 +334,40 @@ function onClick(event) {
         //camera.position.z = object.position.z;
         //camera.position.x = object.position.x;
         //camera.position.Z = object.position.Z;
+        
+        // gsap.to(camera.position,{
+        //     x: object.position.x * 2,
+        //     z: 14,
+        //     duration: 1.5
+        // });
+
+        object.geometry.computeBoundingBox();
+        let boundingBox = object.geometry.boundingBox;
+        const meshPosition = new THREE.Vector3();
+        meshPosition.subVectors(boundingBox.max, boundingBox.min);
+        meshPosition.multiplyScalar(0.5);
+        meshPosition.add(boundingBox.min);
+        meshPosition.applyMatrix4(object.matrixWorld);
+
         gsap.to(camera.position,{
-            x: object.position.x * 2,
-            z: 14,
+            x: meshPosition.x + 50,
+            y: meshPosition.y + 50,
+            z: meshPosition.z + 50,
             duration: 1.5
         });
+    
+        gsap.to(controls.target,{
+            x: meshPosition.x,
+            y: meshPosition.y,
+            z: meshPosition.z,
+            duration: 1.5,
+            onUpdate: function() {
+                controls.update();
+            }
+        });
+        // controls.target.setX(meshPosition.x);
+        // controls.target.setY(meshPosition.y);
+        // controls.target.setZ(meshPosition.z);
 
         if (pauseBtn.classList.contains('stop')) {
             planetInfo.classList.remove("none");
@@ -364,9 +393,28 @@ pauseBtn.addEventListener('click', function() {
         pauseBtn.innerHTML = '<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />';
       }
 })
+document.querySelector('.close-btn').addEventListener("click", () => {
+    gsap.to(camera.position, {
+        x: 0,
+        y: 300,
+        z: 500,
+        duration: 1.5
+    });
+
+    gsap.to(controls.target, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: 1.5,
+        onUpdate: function() {
+            controls.update();
+        }
+    });
+    planetInfo.classList.add("none");
+});
 
 function togglePause() {
-    pauseBtn.classList.toggle('stop');
+    pauseBtn.classList.add('stop');
 
     if (pauseBtn.classList.contains('stop')) {
         pauseBtn.innerHTML = '<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />';
